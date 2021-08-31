be5fb3a0614f983b93fed1894548326a5ac99b5f
Noah Holt <tidalturtles@gmail.com>
2021-08-31 11:50:06 -0600

    BackBones
    Set up the basic back bones of the other things I need to do.
    
    Next need to figure out how to do them.

--- HoltNoahAssignment1.java
+++ HoltNoahAssignment1.java
@@ -1,4 +1,8 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class HoltNoahAssignment1 {
 
@@ -16,26 +20,75 @@ public class HoltNoahAssignment1 {
         int[] array1 = {13,18, 1, 4, 8, 16, 11};
         int[] array2 = {9, 10, 5, 12, 3, 7, 14};
 
+        //print out arrays
+
         System.out.println("Array1: original");
         System.out.println("--------------------------");
+
         for (int i = 0; i < array1.length; i++) {
 
             System.out.print("array1[" + i + "] = ");
             System.out.println(array1[i]);
 
-        }
+        } // for loop
 
         System.out.println("");
         System.out.println("");
         System.out.println("Array2: original");
         System.out.println("--------------------------");
+
         for (int i = 0; i < array2.length; i++) {
 
             System.out.print("array2[" + i + "] = ");
             System.out.println(array2[i]);
 
-        }
+        } //for loop
 
-    }
+        //sort through and reassign arrays.
+
+        //find out how to reassign and place here.
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+
+        //print out new arrays
+
+        System.out.println("Array1: Sorted with Even Values");
+        System.out.println("--------------------------");
+
+        for (int i = 0; i < array1.length; i++) {
+
+            System.out.print("array1[" + i + "] = ");
+            System.out.println(array1[i]);
+
+        } // for loop
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Array2: Sorted with Odd Values");
+        System.out.println("--------------------------");
+
+        for (int i = 0; i < array2.length; i++) {
+
+            System.out.print("array2[" + i + "] = ");
+            System.out.println(array2[i]);
+
+        } //for loop
+        
+        //writing the file
+
+        File orderdList = new File("assignment1.txt");
+        PrintWriter outputList = new PrintWriter(orderdList);
+
+        //reading the file
+
+        Scanner readFile = new Scanner(orderdList);
+
+
+        outputList.close();
+        readFile.close();
+
+    } //main
     
-}
+} //assignment class 
