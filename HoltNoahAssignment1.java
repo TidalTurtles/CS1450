0e7feaf343bf8735127b3053e87713a174e54a6c
Noah Holt <tidalturtles@gmail.com>
2021-08-31 10:26:18 -0600

    Initial commit
    sorting out how to do things, still unsure about a lot but writing what I can

---
+++ HoltNoahAssignment1.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class HoltAssignment1 {
+
+	public static void main(String[] args) throws IOException{
+		/*Noah Holt
+		 * CS1450 Data Structures
+		 * T/Thr section
+		 * Due 2 Sept
+		 * Assignment 1
+		 * This assignment is to refresh on what was forgotten after last semester
+         * we will hard code two arrays and then minipulate them and write them to 
+         * a file, and lastly read that file into a new array.
+		 */
+
+        int[] array1 = {13,18, 1, 4, 8, 16, 11};
+        int[] array2 = {9, 10, 5, 12, 3, 7, 14};
+
+        System.out.println("Array1: original");
+        for (i = 0; i < array1.length; i++) {
+
+            System.out.println(array1[i]);
+
+        }
+
+    }
+    
+}
