b7cd93ab7a608b588e6373af3bd0f69d71fb79bc
Noah Holt <tidalturtles@gmail.com>
2021-09-09 16:24:42 -0600

    Something
    Not really sure what I did, says there is a problem with my subclasses but i dont see it and it is making me upset

--- HoltNoahAssignment2.java
+++ HoltNoahAssignment2.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HoltNoahAssignment2{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         /*Noah Holt
 		 * CS1450 Data Structures
@@ -18,78 +20,154 @@ public class HoltNoahAssignment2{
 		File actorList = new File("Actors.txt");
 		Scanner readActors = new Scanner(actorList);
 
+		int numActors = readActors.nextInt();
 
-    }
+		Actor[] actors = new Actor[numActors];
 
-}
+		// lets create our Actors and store in an array
+		for(int i = 0; i < actors.length; i++) {
 
-class actor {
+			String type = readActors.next();
+			String name = readActors.nextLine();
+
+			switch(type) {
+
+				case "Hero":
+				Hero aHero = new Hero(name);
+				actors[i] = aHero;
+				break;
+
+				case "Villain":
+				Villain aVillain = new Villain(name);
+				actors[i] = aVillain;
+				break;
+
+				case "Monster":
+				Monster aMonster = new Monster(name);
+				actors[i] = aMonster;
+				break;
+
+				case "Droid":
+				Droid aDroid = new Droid(name);
+				actors[i] = aDroid;
+				break;
+
+			}
+
+		}
+
+		readActors.close();
+
+    } //main
+
+} //assignment
+
+
+class Actor {
 
 	private String type;
 	private String name;
 
-	public actor(String type, String name) {
+	public Actor(String type, String name) {
 
+		this.type = type;
+		this.name = name;
 
-
-	}
+	} //actor constructor
 
 	public String getType() {
 
 		return type;
 
-	}
+	} //getType
 
 	public String getName() {
 
 		return name;
 
-	}
+	} //getName
 
 	public String motto() {
 
 		return "I am an Actor";
 
-	}
+	} //default motto
 
 }
 
-class hero extends actor {
+class Hero extends Actor {
 
-	public hero(String name) {
+	public Hero(String name) {
 
 		
 
-	}
+	} //hero
 
 	public String motto() {
 
 		return "To the rescue! KAPOW!! BAM!!! POW!!!!";
 
-	}
+	} //hero motto
 
-}
+} //hero
 
-class villain extends actor {
+class Villain extends Actor {
+
+	public Villain(String name) {
+
+		
+
+	} //villian
+
+	public String motto() {
+
+		return "You'll never stop me! HAHAHAHA!!";
+
+	} //villian motto
+
+} //villain
+
+class Monster extends Actor {
+
+	public Monster(String name) {
 
 
 
-}
+	} //monster
 
-class monster extends actor {
+	public String motto() {
+
+		return "RRAAAUGGHH GRROWR!!!!";
+
+	} //monster motto
+
+} //monster
+
+class Droid extends Actor {
+
+	private String name;
+
+	public Droid(String name) {
 
 
 
-}
+	} //droid
 
-class droid extends actor {
+	public String motto() {
+
+		return "Beep Beep Bloop Boop Beep!";
+
+	} //droid motto
+
+} //droid
+
+class Movie {
+
+	public Movie() {
 
 
 
-}
-
-class movie {
-
-
+	} //movie
     
-}
+} //movie
+
