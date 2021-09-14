ccd903768098583dd79d0fa2abdbacb756923d27
Noah Holt <tidalturtles@gmail.com>
2021-09-14 11:17:06 -0600

    im back
    needed to find charging station

---
+++ HoltNoahAssignment3.java
@@ -0,0 +1,70 @@
+public class HoltNoahAssignment3 {
+    
+    public static void main(String[] args) {
+
+        //need array list
+        //need open file and place in array list
+        //call displayBirds
+        //call findSwimmers
+        //call swimmingRace
+
+    } //main
+
+} //assignment
+
+class bird { //make this abstract
+
+    private String type;
+    private String name;
+
+    public String getType() {
+
+        return type;
+
+    }
+
+    public String getName() { 
+
+        return name;
+
+    }
+
+    //abstract method strangeFact();
+
+} //bird
+
+class Penguin {
+
+
+
+} //penguin
+
+class Ostrich {
+
+
+
+} //Ostrich
+
+class Duck {
+
+
+
+} //duck
+
+class SootyTern {
+
+
+
+} //SootyTern
+
+class Loon {
+
+
+
+} //loon
+
+class HummingBird {
+
+
+
+} //HummingBird
