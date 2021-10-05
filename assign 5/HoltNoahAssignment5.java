7aab0d6c590556e478e5baea1d03848fe7c07736
Noah Holt <tidalturtles@gmail.com>
2021-10-05 09:53:08 -0600

    laid out basic skeleton
    skeleton created for this assignment.

---
+++ assign 5/HoltNoahAssignment5.java
@@ -0,0 +1,66 @@
+
+import java.io.IOException;
+
+public class HoltNoahAssignment5 {
+
+    public static void main(String[] args) throws IOException{
+
+
+
+    } //main method
+
+    public static double findAverage() {
+
+        double result = 0; //filler
+        return result; //filler
+
+    } //find average
+
+    public static void printStack() {
+
+
+
+    } //print stack
+
+} //assignment class
+
+//for part 2 of assignment
+
+class GenericStack<E> {
+
+    public GenericStack() {
+
+
+    } //generic stack method
+
+    public boolean isEmpty() {
+
+        return false; //filler
+
+    } // is empty method
+
+    public int getSize() {
+
+        return 0; //filler
+
+    } // get size method
+
+    public E peek() {
+
+        
+
+    } //peek method
+
+    public E pop() {
+
+
+
+    } // pop method
+
+    public void push () {
+
+
+
+    } // push method
+
+} //generic stack class
