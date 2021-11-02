56fc8c7217cd1be9584caa7bad0bbce6a07bbedb
Noah Holt <tidalturtles@gmail.com>
2021-11-02 11:29:23 -0600

    starting up 8
    Starting up assignment 8, downloaded all needed files as well as started the new java document

---
+++ assign 8/HoltNoahAssignment8.java
@@ -0,0 +1,34 @@
+
+/* Noah Holt
+ * CS1450 Data Structures
+ * T/Thr section
+ * Due 4 Nov
+ * Assignment 8
+ * This assignment is a Decoder to find out the mystery of the parrots
+ * next location that we will use to help us explore further Array Lists,
+ * Queues, Stacks as well as start our understandings of how to use iterators.
+ */
+
+ // copied over all imports from assignment 7, probably won't need all 
+ // but might need some more too.
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class HoltNoahAssignment8 {
+
+    public static void main(String[] args) throws IOException {
+
+        
+
+    } // main method
+
+} //assignment class
+
