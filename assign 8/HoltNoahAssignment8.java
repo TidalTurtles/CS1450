e5d566aeedc0f77e7d530ba7283b518ae0490f5f
Noah Holt <tidalturtles@gmail.com>
2021-11-03 09:20:05 -0600

    constructed the classes
    built the main classes and the methods they will use so that it is all ready to rock once I sit down to actually begin

--- assign 8/HoltNoahAssignment8.java
+++ assign 8/HoltNoahAssignment8.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -32,3 +33,69 @@ public class HoltNoahAssignment8 {
 
 } //assignment class
 
+// class that will do all the work
+class SecretTranslator {
+
+    //private data fields
+    private Stack myStack;
+
+    //constructor method
+    public SecretTranslator() {
+
+        this.myStack = new Stack();
+
+    } //constructor
+
+    // actually decodes the message in 2 steps
+    //step 1: traverse the key to see where to get the char
+    //step 2: undo the reversal of the message from where it was put on the stack
+    public String decode(Iterator<Character> msg1Iterator, Iterator<Integer> msg2Interator, Iterator<Integer> keyIterator) {
+
+        //example for/if loop in assignment notes
+        //step 1
+
+        return "Hello"; //placeHolder
+
+    }
+
+} //secret translator class
+
+//class defining what our stack can do
+class Stack {
+
+    //private data fields
+    private ArrayList<Character> storage;
+
+    //constructor method
+    public Stack() {
+
+        this.storage = new ArrayList<Character>();
+
+    } // constructor method
+
+    //is empty method (return bool)
+    public boolean isEmpty() {
+        if(storage.isEmpty() == true) {
+            return true;
+        } else {
+            return false;
+        } //if empty
+    } //is empty method
+
+    //get size of stack
+    public int getSize() {
+        return storage.size();
+    } //get size method
+
+    //push: add element to the stack
+    public void push(Character value) {
+        storage.add(value);
+    } // push method 
+
+    //pop: remove and return
+    public char pop() {
+        int last = storage.size();
+        return (char)storage.remove(last-1);
+    } //pop method
+
+} // stack class
