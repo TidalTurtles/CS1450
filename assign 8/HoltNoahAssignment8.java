4d06a43f68724dd9ee2f0d491bcb7a2201b2bbc3
Noah Holt <tidalturtles@gmail.com>
2021-11-03 14:32:36 -0600

    Nice
    all finished with some time to spare

--- assign 8/HoltNoahAssignment8.java
+++ assign 8/HoltNoahAssignment8.java
@@ -14,11 +14,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.LinkedList;
@@ -27,7 +24,109 @@ public class HoltNoahAssignment8 {
 
     public static void main(String[] args) throws IOException {
 
+        // 2 step proccess
+
+        //step 1 (known message) w/ Array Lists
+        //open files
+        File charFile = new File("arrayMessage1.txt");
+        File intFile = new File("arrayMessage2.txt");
+        File keyFile = new File("arrayKey.txt");
+
+        //scanners for files
+        Scanner getChars = new Scanner(charFile);
+        Scanner getAscii = new Scanner(intFile);
+        Scanner getKey = new Scanner(keyFile);
+
+        //declare array lists
+        ArrayList<Character> charArray = new ArrayList<>();
+        ArrayList<Integer> intArray = new ArrayList<>();
+        ArrayList<Integer> keyArray = new ArrayList<>();
+
+        //making the arrayLists
+        //first char files
+        String decodeMe = getChars.nextLine();
+        for(int i = 0; i < decodeMe.length(); i++) {
+            charArray.add(decodeMe.charAt(i));
+        }
+
+        //ascii values
+        while(getAscii.hasNext()) {
+            intArray.add(getAscii.nextInt());
+        } //adds till completion
+
+        //key values
+        while(getKey.hasNext()) {
+            keyArray.add(getKey.nextInt());
+        }
         
+        //create iterators
+        Iterator<Character> arrayListIterator1 = charArray.iterator();
+        Iterator<Integer> arrayListIterator2 = intArray.iterator();
+        Iterator<Integer> arrayListIteratorKey = keyArray.iterator();
+
+        //time to decode this message
+        //make decoder
+        SecretTranslator arrayListMessage = new SecretTranslator();
+        //use decoder
+        String message1 = arrayListMessage.decode(arrayListIterator1, arrayListIterator2, arrayListIteratorKey);
+
+        System.out.println("the secret string 1 is: " + message1);
+
+        //dont forget to close files
+        getChars.close();
+        getAscii.close();
+        getKey.close();
+
+        //step 2 (mystery message) w/ queues
+        //open files
+        File charFile2 = new File("queueMessage1.txt");
+        File intFile2 = new File("queueMessage2.txt");
+        File keyFile2 = new File("queueKey.txt");
+
+        //scanners for files
+        Scanner getChars2 = new Scanner(charFile2);
+        Scanner getAscii2 = new Scanner(intFile2);
+        Scanner getKey2 = new Scanner(keyFile2);
+
+        //declare array lists
+        Queue<Character> charQueue = new LinkedList<>();
+        Queue<Integer> intQueue = new LinkedList<>();
+        Queue<Integer> keyQueue = new LinkedList<>();
+
+        //making the arrayLists
+        //first char files
+        String decodeMe2 = getChars2.nextLine();
+        for(int i = 0; i < decodeMe2.length(); i++) {
+            charQueue.add(decodeMe2.charAt(i));
+        }
+
+        //ascii values
+        while(getAscii2.hasNext()) {
+            intQueue.add(getAscii2.nextInt());
+        } //adds till completion
+
+        //key values
+        while(getKey2.hasNext()) {
+            keyQueue.add(getKey2.nextInt());
+        }
+        
+        //create iterators
+        Iterator<Character> queueIterator1 = charQueue.iterator();
+        Iterator<Integer> queueIterator2 = intQueue.iterator();
+        Iterator<Integer> queueIteratorKey = keyQueue.iterator();
+
+        //time to decode this message
+        //make decoder
+        SecretTranslator queueMessage = new SecretTranslator();
+        //use decoder
+        String message2 = queueMessage.decode(queueIterator1, queueIterator2, queueIteratorKey);
+
+        System.out.println("the secret string 2 is: " + message2);
+
+        //dont forget to close files
+        getChars2.close();
+        getAscii2.close();
+        getKey2.close();
 
     } // main method
 
@@ -51,10 +150,26 @@ class SecretTranslator {
     //step 2: undo the reversal of the message from where it was put on the stack
     public String decode(Iterator<Character> msg1Iterator, Iterator<Integer> msg2Interator, Iterator<Integer> keyIterator) {
 
-        //example for/if loop in assignment notes
         //step 1
+        while(keyIterator.hasNext()) {
+            int fromKey = keyIterator.next();
+            if(fromKey == 0) {
+                myStack.push(msg1Iterator.next());
+            } else {
+                int changeMe = msg2Interator.next();
+                char pushMe = (char)changeMe;
+                myStack.push(pushMe);
+            } // if one or zero
+        } //while loop has next
 
-        return "Hello"; //placeHolder
+        //now turn the stack into a string
+        char[] secretMessage = new char[myStack.getSize()];
+        for(int i = 0; i < secretMessage.length; i++) {
+            secretMessage[i] = myStack.pop();
+        }
+        String finalMessage = new String(secretMessage);
+        
+        return finalMessage; //placeHolder
 
     }
 
